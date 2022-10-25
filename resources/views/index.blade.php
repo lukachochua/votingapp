003295cdc19c78ede858d0c208b497229911d3c4
lukachochua <lukachochua@gmail.com>
2022-10-25 20:50:39 +0400

    feat: create an 'add idea form' with tailwind css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -123,17 +123,17 @@
                         </div>
 
                         <div class="flex items-center space-x-2">
-                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
-                                Open
+                            <div class="bg-purple text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
+                                Considering
                             </div>
                             <button class="relative bg-gray-100 hover:bg-gray-200 transition duration-150 ease-in rounded-full h-7 py-2 px-3">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
                                     <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
                                 </svg>
-                                <ul class="absolute w-44 font-semibold bg-white shadow-dialogg rounded-xl py-3 text-left ml-8">
+                                {{-- <ul class="absolute w-44 font-semibold bg-white shadow-dialogg rounded-xl py-3 text-left ml-8">
                                     <li><a href="#" class="hover:bg-gray-100 block px-5 py-3 transition duration-150 ease-in">Mark as Spam</a></li>
                                     <li><a href="#" class="hover:bg-gray-100 block px-5 py-3 transition duration-150 ease-in">Delete Post</a></li>
-                                </ul>
+                                </ul> --}}
                             </button>
                         </div>
                     </div>
@@ -179,17 +179,17 @@
                         </div>
 
                         <div class="flex items-center space-x-2">
-                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
-                                Open
+                            <div class="bg-yellow text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
+                                In Progress
                             </div>
                             <button class="relative bg-gray-100 hover:bg-gray-200 transition duration-150 ease-in rounded-full h-7 py-2 px-3">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
                                     <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
                                 </svg>
-                                <ul class="absolute w-44 font-semibold bg-white shadow-dialogg rounded-xl py-3 text-left ml-8">
+                                {{-- <ul class="absolute w-44 font-semibold bg-white shadow-dialogg rounded-xl py-3 text-left ml-8">
                                     <li><a href="#" class="hover:bg-gray-100 block px-5 py-3 transition duration-150 ease-in">Mark as Spam</a></li>
                                     <li><a href="#" class="hover:bg-gray-100 block px-5 py-3 transition duration-150 ease-in">Delete Post</a></li>
-                                </ul>
+                                </ul> --}}
                             </button>
                         </div>
                     </div>
@@ -235,17 +235,17 @@
                         </div>
 
                         <div class="flex items-center space-x-2">
-                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
-                                Open
+                            <div class="bg-red text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
+                                Closed
                             </div>
                             <button class="relative bg-gray-100 hover:bg-gray-200 transition duration-150 ease-in rounded-full h-7 py-2 px-3">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
                                     <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
                                 </svg>
-                                <ul class="absolute w-44 font-semibold bg-white shadow-dialogg rounded-xl py-3 text-left ml-8">
+                                {{-- <ul class="absolute w-44 font-semibold bg-white shadow-dialogg rounded-xl py-3 text-left ml-8">
                                     <li><a href="#" class="hover:bg-gray-100 block px-5 py-3 transition duration-150 ease-in">Mark as Spam</a></li>
                                     <li><a href="#" class="hover:bg-gray-100 block px-5 py-3 transition duration-150 ease-in">Delete Post</a></li>
-                                </ul>
+                                </ul> --}}
                             </button>
                         </div>
                     </div>
@@ -292,17 +292,17 @@
                         </div>
 
                         <div class="flex items-center space-x-2">
-                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
-                                Open
+                            <div class="bg-green text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
+                                Implemented
                             </div>
                             <button class="relative bg-gray-100 hover:bg-gray-200 transition duration-150 ease-in rounded-full h-7 py-2 px-3">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
                                     <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
                                 </svg>
-                                <ul class="absolute w-44 font-semibold bg-white shadow-dialogg rounded-xl py-3 text-left ml-8">
+                                {{-- <ul class="absolute w-44 font-semibold bg-white shadow-dialogg rounded-xl py-3 text-left ml-8">
                                     <li><a href="#" class="hover:bg-gray-100 block px-5 py-3 transition duration-150 ease-in">Mark as Spam</a></li>
                                     <li><a href="#" class="hover:bg-gray-100 block px-5 py-3 transition duration-150 ease-in">Delete Post</a></li>
-                                </ul>
+                                </ul> --}}
                             </button>
                         </div>
                     </div>
