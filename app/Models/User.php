9fa700703248ee1d437de74e46590f6e633322ff
lukachochua <lukachochua@gmail.com>
2022-11-07 23:36:04 +0400

    feat: transfer to livewire components, display the votes count on both the index and show pages

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,6 +48,11 @@ class User extends Authenticatable
         return $this->hasMany(Idea::class);
     }
 
+    public function votes()
+    {
+        return $this->belongsToMany(Idea::class, 'votes');
+    }
+
     public function getAvatar()
     {
         $firstCharecter = $this->email[0];
