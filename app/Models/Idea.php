6b228cc012a34762cada1d4b34e2ea0cb2bc1d4f
lukachochua <lukachochua@gmail.com>
2022-11-15 20:36:08 +0400

    feat: fix livewire bug that does not exist since version 2.6

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use App\Exceptions\DuplicateVoteException;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
+use App\Exceptions\VoteNotFoundException;
 
 
 class Idea extends Model
@@ -60,7 +62,10 @@ class Idea extends Model
     }
 
     public function vote(User $user) 
-    {
+    {   
+        if($this->isVotedByUser($user)) {
+            throw new DuplicateVoteException;
+        }
         Vote::create([
             'idea_id' => $this->id,
             'user_id' => $user->id
@@ -69,9 +74,13 @@ class Idea extends Model
 
     public function removeVote(User $user) 
     {
-        Vote::where('idea_id', $this->id)
+        $voteToDelete = Vote::where('idea_id', $this->id)
             ->where('user_id', $user->id)
-            ->first()
-            ->delete();
+            ->first();
+        if($voteToDelete) {
+            $voteToDelete->delete();
+        } else {
+            throw new VoteNotFoundException;
+        }
     }
 }
--- tests/Unit/IdeaTest.php
+++ tests/Unit/IdeaTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\Exceptions\DuplicateVoteException;
+use App\Exceptions\VoteNotFoundException;
 use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
@@ -72,6 +74,8 @@ class IdeaTest extends TestCase
         $this->assertTrue($idea->isVotedByUser($user));
     }
 
+    
+
     /** @test */ 
     public function user_can_remove_vote_for_idea()
     {
@@ -98,4 +102,57 @@ class IdeaTest extends TestCase
         $idea->removeVote($user);
         $this->assertFalse($idea->isVotedByUser($user));
     }
+
+    /** @test */ 
+    public function voting_for_an_idea_thats_already_voted_for_throws_exception()
+    {
+        $user = User::factory()->create();
+
+
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'title' => 'My First Idea',
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'description' => 'Description of my first idea'
+        ]);
+        
+        Vote::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $user->id
+        ]);
+
+        $this->expectException(DuplicateVoteException::class);
+
+        $idea->vote($user);
+    }
+
+    /** @test */ 
+    public function removing_a_vote_that_does_not_exception_throws_exception()
+    {
+        $user = User::factory()->create();
+
+
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'title' => 'My First Idea',
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'description' => 'Description of my first idea'
+        ]);
+
+        $this->expectException(VoteNotFoundException::class);
+
+        $idea->removeVote($user);
+    }    
 }
