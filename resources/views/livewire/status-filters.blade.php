40165850ab793c35af96eb7bb498ab5a142e403a
lukachochua <lukachochua@gmail.com>
2022-11-23 23:54:13 +0400

    feat: tests to ensure that the status filters are working as expected

--- resources/views/livewire/status-filters.blade.php
+++ resources/views/livewire/status-filters.blade.php
@@ -1,7 +1,7 @@
 <nav class="hidden md:flex items-center justify-between text-gray-400 text-xs">
     <ul class="flex uppercase font-semibold space-x-10 border-b-4 pb-3">
         <li><a wire:click.prevent="setStatus('All')" href="#" class="border-b-4 pb-3 
-            @if ($status==='All') border-blue text-gray-900 @endif hover:border-blue">All Ideas ( {{$statusCount['all_statuses']}} )</a></li>
+            @if ($status==='All') border-blue text-gray-900 @endif hover:border-blue">All Ideas ({{$statusCount['all_statuses']}})</a></li>
         <li><a wire:click.prevent="setStatus('Considering')" href="#" class="transition duration-150 ease-in border-b-4 pb-3 hover:border-blue 
             @if ($status==='Considering') border-blue text-gray-900 @endif ">Considering ( {{$statusCount['considering']}})</a></li>
         <li><a wire:click.prevent="setStatus('In Progress')" href="#" class="transition duration-150 ease-in border-b-4 pb-3 hover:border-blue 
@@ -12,7 +12,7 @@
 
     <ul class="flex uppercase font-semibold space-x-10 border-b-4 pb-3">
         <li><a wire:click.prevent="setStatus('Implemented')" href="#" class="transition duration-150 ease-in border-b-4 pb-3 hover:border-blue 
-            @if ($status==='Implemented') border-blue text-gray-900 @endif ">implemented ( {{$statusCount['implemented']}})</a></li>
+            @if ($status==='Implemented') border-blue text-gray-900 @endif ">Implemented ({{$statusCount['implemented']}})</a></li>
         <li><a wire:click.prevent="setStatus('Closed')" href="#" class="transition duration-150 ease-in border-b-4 pb-3 hover:border-blue 
             @if ($status==='Closed') border-blue text-gray-900 @endif ">Closed ( {{$statusCount['closed']}})</a></li>
     </ul> 
