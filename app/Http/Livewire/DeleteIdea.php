ee341eb5462531124d44774160b76b4e41de3590
lukachochua <lukachochua@gmail.com>
2022-12-06 22:38:21 +0400

    feat: implement delete idea functionality using livewire

---
+++ app/Http/Livewire/DeleteIdea.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Idea;
+use App\Models\Vote;
+use Livewire\Component;
+use Illuminate\Http\Response;
+
+class DeleteIdea extends Component
+{
+
+    public $idea;
+
+
+    protected $rules = [
+        'title' => 'required|min:4',
+        'category'=> 'required|integer|exists:categories,id',
+        'description' => 'required|min:4'
+    ];
+
+    public function mount(Idea $idea)
+    {
+        $this->idea = $idea;
+
+    }
+
+    public function deleteIdea()
+    {
+        // Auth
+        if(auth()->guest() || auth()->user()->cannot('delete', $this->idea)) {
+            abort(Response::HTTP_FORBIDDEN);
+        }
+
+        Vote::where('idea_id', $this->idea->id)->delete();
+        Idea::destroy($this->idea->id);
+        return redirect(route('idea.index'));
+    }
+
+    public function render()
+    {
+        return view('livewire.delete-idea');
+    }
+}
