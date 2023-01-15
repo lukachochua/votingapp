0cc1deb83e168baad78273179492c92a6b7844f6
lukachochua <lukachochua@gmail.com>
2023-01-15 22:58:56 +0400

    feat: add a badge count and sketelon loaden when notifications are being loaded' ;

--- app/Http/Livewire/CommentNotifications.php
+++ app/Http/Livewire/CommentNotifications.php
@@ -6,18 +6,33 @@ use Livewire\Component;
 
 class CommentNotifications extends Component
 {
+    const NOTIFICATION_THRESHOLD = 20;
     public $notifications;
+    public $notificationCount;
+    public $isLoading;
 
     protected $listeners = ['getNotifications'];
 
     public function mount()
     {
         $this->notifications = collect([]);
+        $this->isLoading = true;
+        $this->getNotificationcount();
+    }
+
+    public function getNotificationCount()
+    {
+        $this->notificationCount = auth()->user()->unreadnotifications()->count();
+
+        if($this->notificationCount > self::NOTIFICATION_THRESHOLD) {
+            $this->notificationCount = self::NOTIFICATION_THRESHOLD.'+';
+        }
     }
 
     public function getNotifications()
     {
-        $this->notifications =  auth()->user()->unreadNotifications;
+        $this->notifications =  auth()->user()->unreadNotifications()->latest()->take(self::NOTIFICATION_THRESHOLD)->get();
+        $this->isLoading = false;
     }
     public function render()
     {
